599e159ee4a7151c707e07d67720ceaaacf08c71
chem is try <96931510+Bro-Xun@users.noreply.github.com>
2022-02-13 23:21:00 +0800

    Add files via upload

--- assets/js/aplayer_config.js
+++ assets/js/aplayer_config.js
@@ -918,7 +918,7 @@ const ap = new APlayer({
             name: '工具人之歌',
             artist: 'Potter King',
 			cover:'assets/img/45.jpg',
-            url: 'https://cdn.jsdelivr.net/gh/bro-xun/oldpage/assets/music/45.m4a',
+            url: 'https://cdn.jsdelivr.net/gh/bro-xun/oldpage/assets/music/45.mp3',
 			lrc:'assets/lyrics/45.lrc',
             theme: 'pink'
         }
